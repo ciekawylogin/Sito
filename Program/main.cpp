89292f34b3c05c6c5d66a143752566a245afd10b
MateuszMateo <k.mateusz120@gmail.com>
2015-11-17 19:23:28 +0100

    Zmiana typu liczbowego tablicy z "bool" na "int"

--- Program/main.cpp
+++ Program/main.cpp
@@ -3,36 +3,35 @@
 
 using namespace std;
 
+
+
 int main() {
   clock_t begin = clock();
 
   /// poczatek programu
+
+
   int n;
   cin >> n;
-  bool tablica[n+1];
+  int tablica[n+1];
   for(int i=0;i<n;i++){
       cin>>tablica[i];
-  }
-  int Tak, Nie;
-  for(int l=2,j=0;j<n;j++,l++){
-      for(int k=2*l;k*k<=n;k=k+l){
-      if(tablica[j]==l)
-          tablica[j]=Tak;
-      if(tablica[j]==Tak)
-          tablica[j]=Tak;
-      if(tablica[j]==Nie)
-          tablica[j]=Nie;
-      if(tablica[j]==k)
-          tablica[j]= Nie;
-      }
- }
+      for(int j=2;j*j<=n;j++){
+          if(tablica[i]%j==0)
+              tablica[i]=0;
+          else
+              tablica[i]=1;
 
- for(int i=0;i<n;i++){
-      if(tablica[i]==Tak){
-          cout<<"Nie";}
-      else
-          cout<<"Tak";
+      }
   }
+
+  for(int i=0;i<n;i++){
+      if(tablica[i]==0)
+          cout<<"Nie"<<endl;
+      if(tablica[i]==1)
+          cout<<"Tak"<<endl;
+  }
+
   /// koniec programu
 
   clock_t end = clock();
@@ -40,4 +39,3 @@ int main() {
 
   cout << "czas dzialania: " << elapsed_secs << " sekund." << endl;
 }
-
